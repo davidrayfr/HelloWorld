a379792dfc91fae8a2c83012df0b0c8bfe5a687a
davidrayfr <davidrayfr.gmail.com>
2017-01-20 14:37:08 +0100

    That is a test for CheckLCD

---
+++ checklcd/checklcd.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_TFTLCD.h>
+
+void setup() {
+  // put your setup code here, to run once:
+// find the TFT display
+uint16_t identifier = tft.readRegister(0x0);
+if (identifier == 0x9325) {
+   Serial.println("Found ILI9325");
+} else if (identifier == 0x9328) {
+   Serial.println("Found ILI9328");
+} else {
+   Serial.print("Unknown driver chip ");
+   Serial.println(identifier, HEX);
+   while (1);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
