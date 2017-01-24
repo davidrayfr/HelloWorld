8ee1cefea3beb37a86b463924adbee24f09f164b
davidrayfr <davidrayfr.gmail.com>
2017-01-24 16:18:27 +0100

    test branch

---
+++ BME280_Test.ino
@@ -0,0 +1,27 @@
+// I2C BME280
+// by David Ray
+// 2017
+
+BME_280_Adress byte(0x77)
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(); // join i2c bus (address optional for master)
+}
+
+byte val = 0;
+
+void loop() {
+  Wire.beginTransmission(44); // transmit to device #44 (0x2c)
+  // device address is specified in datasheet
+  Wire.write(byte(0x00));            // sends instruction byte
+  Wire.write(val);             // sends potentiometer value byte
+  Wire.endTransmission();     // stop transmitting
+
+  val++;        // increment value
+  if (val == 64) { // if reached 64th position (max)
+    val = 0;    // start over from lowest value
+  }
+  delay(500);
+}
